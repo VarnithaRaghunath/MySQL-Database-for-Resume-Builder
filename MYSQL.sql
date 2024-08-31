3e300ebcf8265bcb923b8845203723ceb9329676
VarnithaRaghunath <150449637+VarnithaRaghunath@users.noreply.github.com>
2024-08-31 22:43:51 +0530

    Update MYSQL.sql

--- MYSQL.sql
+++ MYSQL.sql
@@ -1,1 +1,9 @@
-create database welcome;
+CREATE DATABASE greetings;
+USE greetings;
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(255) NOT NULL
+);
+
+INSERT INTO messages (message) VALUES ('Welcome');
