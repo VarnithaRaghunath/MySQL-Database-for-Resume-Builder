e23e034048620930f030229345c6ea3b84eaaafb
VarnithaRaghunath <150449637+VarnithaRaghunath@users.noreply.github.com>
2024-08-31 22:44:13 +0530

    Delete MYSQL.sql

--- MYSQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE greetings;
-USE greetings;
-
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    message VARCHAR(255) NOT NULL
-);
-
-INSERT INTO messages (message) VALUES ('Welcome');
